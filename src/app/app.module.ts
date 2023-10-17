6c42517ad9ae4e7927bb178afd41a5a233e06fb5
Aena <annu.cuty@gmail.com>
2023-10-17 23:13:48 +0530

    Publishing to GitHub at 23.13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
@@ -9,6 +10,20 @@ import { SupportComponent } from './support/support.component';
 import { LandingImageComponent } from './landing-image/landing-image.component';
 import { LandingDescriptionComponent } from './landing-description/landing-description.component';
 import { LandingStatementComponent } from './landing-statement/landing-statement.component';
+import { HomeComponent } from './home/home.component';
+import { TechnicalComponent } from './technical/technical.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes : Routes = [
+  {path: '', redirectTo: '/home',pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'technical/rp', component: TechnicalComponent},
+  {path: 'technical/tsc', component: TechnicalComponent},
+  {path: 'technical/tss', component: TechnicalComponent},
+  {path: 'technical/cc', component: TechnicalComponent},
+  {path: 'technical/tco', component: TechnicalComponent},
+  {path: '**', component: PageNotFoundComponent}
+]
 
 @NgModule({
   declarations: [
@@ -19,10 +34,14 @@ import { LandingStatementComponent } from './landing-statement/landing-statement
     SupportComponent,
     LandingImageComponent,
     LandingDescriptionComponent,
-    LandingStatementComponent
+    LandingStatementComponent,
+    HomeComponent,
+    TechnicalComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
